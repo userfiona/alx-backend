817fca9a28a9b76ee32390619e25f3a049884729
userfiona <fionaramaesela@gmail.com>
2024-01-31 16:45:16 +0000

    minigate error and prevent cratching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Basic dictionary"""
+
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+
+
+
+   def put(self, key, item): 
+
+    # Assign to the dictionary self.cache_data the item value for the key key
+
+   self.cache_data = self.catch_data(key)
+
+   # If key or item is None 
+   catch_data = self.cache_data(key)
